737aaa92e13cbe724e7b602d149c873a0cfd88c2
IoanaAlbisoru <oanna.albisoru@gmail.com>
2018-03-17 19:48:50 +0200

    added get file function

--- Client.c
+++ Client.c
@@ -14,77 +14,123 @@
 #define SERVER_PORT 6000
 #define MAXBUF 1024
 
-int main(int argc, char *argv[]){
-    int fd, sockfd;
+char *errorcodes[] = {
+    "1 Succes\n",
+    "2 La revedere\n",
+    "3 Eroare la citire\n",
+    "4 Eroare la crearea fisierului\n",
+    "5 Eroare la scriere\n",
+    "6 EOF prematur\n",
+    "7 Linia este prea lunga\n",
+    "8 Comanda necunoscuta\n",
+    "9 Fisier inexistent\n"
+};
+
+static inline void reply(int sockfd, int code){
+    (void)write(sockfd, errorcodes[code], strlen(errorcodes[code]));
+}
+
+int main(int argc, char * argv[]){
+    int fd, sockfd[argc-3];
     char buf[MAXBUF];
     struct sockaddr_in local_addr, remote_addr;
     int nread, ret;
-    char *delimitator = " \r\n";
-    int ack;
+    unsigned int serverCount = 0;
+    char *delimitator = "\n";
+    int sockfdIndex = 0, argvServerIndex = 2;
     
-    if(argc < 3 || argc > 4){
-        printf("%s adresa fisier [nume] \n", argv[0]);
+    if(argc < 4) {
+        printf("Mod de apel: %s <nume_fisier> <lista_servere> <nr_segmente> \n", argv[0]);
         exit(1);
     }
     
-    if((fd = open(argv[2], O_RDONLY)) == -1){
-        printf("Eroare la deschiderea fisierului %s \n", argv[1]);
-        exit(1);
+    //numarul de servere date
+    for(int i = 2; i < argc - 1; ++i) //porneste dupa nume_fisier pana la nr_segmente, exclusiv
+        serverCount++;
+    
+    printf("%d\n",serverCount);
+    //se creaza un socket diferit pentru fiecare conexiune(client-server) in parte
+    while(sockfdIndex < serverCount){
+        
+        if((sockfd[sockfdIndex] = socket(PF_INET, SOCK_STREAM, 0)) == -1){
+            printf("Eroare la socket \n");
+            exit(1);
+        }
+        set_addr(&local_addr, NULL, INADDR_ANY, 0);
+        
+        if(bind(sockfd[sockfdIndex], (struct sockaddr *)&local_addr, sizeof(local_addr)) == -1){
+            printf("Eroare la bind \n");
+            exit(1);
+        }
+        set_addr(&remote_addr, argv[argvServerIndex], 0, SERVER_PORT);
+        
+        if(connect(sockfd[sockfdIndex], (struct sockaddr *)&remote_addr, sizeof(remote_addr)) == -1){
+            printf("Eroare la conectare \n");
+            exit(1);
+        }
+        printf("Urmeaza sa intreb serverul daca are fisierul.\n");
+        //intreb serverul daca are fisierul
+        
+            //scriu in buffer codul (search) si numele fisierului pe care doresc sa-l caut
+            snprintf(buf, MAXBUF, "search %s%s", argv[1], delimitator);
+            printf("%s \n", buf);
+            //scriu in socket informatia
+            stream_write(sockfd[sockfdIndex], (void *)buf, MAXBUF);
+            printf("s-a intors din stream_write.\n");
+            //astept ca serverul sa raspunda comenzii
+            ret = readline(sockfd[sockfdIndex], buf, MAXBUF);
+            printf("ret: %d \n", ret);
+            
+            //inchide conexiunea daca fisierul nu a fost gasit
+            if(ret != EX3_SUCCESS){
+                    close(sockfd[sockfdIndex]);
+                    sockfdIndex++;
+                    argvServerIndex++;
+                    continue; //sari la urmatorul server
+            }
+            
+        //sfarsit confirmare fisierul
+        
+        
+        //scriu in buffer codul(get) si numele fisierului pe care il doresc
+        snprintf(buf, MAXBUF, "get %s%s", argv[1], delimitator);
+        
+        //scriu in socket comanda
+        stream_write(sockfd[sockfdIndex], (void *)buf, MAXBUF);
+        
+        //astept raspunsul serverului
+        ret = readline(sockfd[sockfdIndex], buf, MAXBUF);
+        
+        if(ret != EX3_SUCCESS){
+            printf("Client: Eroare raspuns. \n");
+            exit(1);
+        }
+        //preluarea fisierului
+            fd = open(argv[1], O_WRONLY | O_CREAT); 
+    
+            if(fd == -1){
+                reply(sockfd[sockfdIndex], EX3_FILECREA);
+                return;
+            }
+            
+            //citirea fisierului
+            while((nread = stream_read(sockfd[sockfdIndex], (void *)buf, MAXBUF)) > 0){
+                if(write(fd, (void *)buf, nread) == -1){
+                    reply(sockfd[sockfdIndex], EX3_FILEWRERR);
+                    return;
+                }
+            }
+            close(fd);
+            if(nread < 0)
+                reply(sockfd[sockfdIndex], EX3_READERR);
+            else
+                reply(sockfd[sockfdIndex], EX3_SUCCESS);
+        //sfarsitul preluarii
+        
+        sockfdIndex++;
+        argvServerIndex++;
     }
     
-    if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1){
-        printf("Eroare la socket \n");
-        exit(1);
-    }
-    set_addr(&local_addr, NULL, INADDR_ANY, 0);
+    return 0;
     
-    if(bind(sockfd, (struct sockaddr *)&local_addr, sizeof(local_addr)) == -1){
-        printf("Eroare la bind \n");
-        exit(1);
-    }
-    set_addr(&remote_addr, argv[1], 0, SERVER_PORT);
-    
-    if(connect(sockfd, (struct sockaddr *)&remote_addr, sizeof(remote_addr)) == -1){
-        printf("Eroare la conectare \n");
-        exit(1);
-    }
-    
-    snprintf(buf, MAXBUF, "filename %s%s", argv[argc-1], delimitator);
-    stream_write(sockfd, (void *)buf, strlen(buf));
-    ret = readline(sockfd, buf, MAXBUF);
-    if(ret != EX3_SUCCESS){
-        printf("Client: Eroare raspuns \n");
-        exit(1);
-    }
-    ack = atoi(buf);
-    switch(ack) {
-        case EX3_SUCCESS: break;
-       default: printf("%s\n", buf); exit(1);
-    }
-    snprintf(buf, MAXBUF, "data%s", delimitator);
-    stream_write(sockfd, (void *)buf, strlen(buf));
-    ret = readline(sockfd, buf, MAXBUF);
-    
-    if(ret != EX3_SUCCESS){
-        printf("Client: Eroare raspuns \n");
-        exit(1);
-    }
-    ack = atoi(buf);
-    switch(ack){
-        case EX3_GOAHEAD: break;
-        default: printf("%s \n", buf); exit(1);
-    }
-    
-    while((nread = read(fd, (void *)buf, MAXBUF)) > 0){
-        stream_write(sockfd, (void *)buf, nread);
-    }
-    if(nread < 0) {
-        printf("Client: Eroare la citire din fisier \n");
-        exit(1);
-    }
-    shutdown(sockfd, SHUT_WR);
-    close(fd);
-    readline(sockfd, buf, MAXBUF);
-    printf("%s \n", buf);
-    exit(0);
 }
